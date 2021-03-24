10c119c24bcc7b7ac33c550e5a18566c99175037
GiovanniZotta <giovannizotta771@gmail.com>
2021-03-24 11:18:39 +0100

    Small refactoring

--- 51attack.py
+++ 51attack.py
@@ -1,11 +1,12 @@
-network_hashrate= 193 * 10**18 # 168 EH/s
+network_hashrate= 193 * 10**18 # (H/s) 193 EH/s (all-time-high network hashrate)
 power = 51 # share of the hashrate the attacker wants to control
 network_power = 100 - power
-kwh_price = 0.01 # suppose 0.01$ for 1KWh (could be much less)
 total_attacker_hashrate = (network_hashrate / network_power) * power
 # the following number should take into account the "discount" that a large scale ASIC production has
-cost_HS = 5000 / 10**(14) # cost of 1 H/S. Based on consumer price antminer s19j => rounded to 100TH/s for $5000(3KWh)
+hardware_cost_1HS = 5000 / 10**14 # cost of 1 H/s. Based on consumer price antminer s19j => rounded to 100TH/s for $5000(3KWh)
+
 rewrite = 6 # number of blocks to rewrite
+kwh_price = 0.01 # suppose 0.01$ for 1KWh (could be much less)
 
 
 def get_reach_time():
@@ -23,13 +24,13 @@ def get_reach_time():
     return hours
 
 def get_electricity_cost_per_hour():
-    consumption_per_hour = 3 * total_attacker_hashrate / 10 ** (14) # (KWh) an ASIC that produces 100TH/s and consumes 3KWh
+    consumption_per_hour = 3 * total_attacker_hashrate / 10**14 # (KWh) an ASIC that produces 100TH/s and consumes 3KWh
     # this looks pretty conservative, as it approximates at around 45TWh per year, while various sources report 80-120TWh per year (maybe because this ASIC is rather recent)
     # print(f"Consumption per hour: {consumption_per_hour/10**6} GWh")
     return consumption_per_hour * kwh_price 
 
 def main():
-    hardware_cost = total_attacker_hashrate * cost_HS
+    hardware_cost = total_attacker_hashrate * hardware_cost_1HS
     print(f"Cost of hardware manufacturing: ${round(hardware_cost/10**6, 2)} M")
     electricity_cost_per_hour = get_electricity_cost_per_hour()
     print(f"Cost of electricity per hour: ${round(electricity_cost_per_hour, 2)}")
